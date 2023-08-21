6788b980c7dbcc1ce3252ece23ed352951e3d093
Yunus Emre Sayın <87127328+YunusEmreSayin@users.noreply.github.com>
2023-08-21 08:55:50 +0300

    Update PersonelController.cs

--- PersonelAPI/Controllers/PersonelController.cs
+++ PersonelAPI/Controllers/PersonelController.cs
@@ -1,38 +1,91 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using PersonelAPI.Model;
 using System.Data.SqlClient;
+using System.Text.Json;
 
 namespace PersonelAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PersonelController : ControllerBase
+    public class getPersonelController : ControllerBase
     {
         public readonly IConfiguration _configuration;
-        public PersonelController(IConfiguration configuration)
+        public getPersonelController(IConfiguration configuration)
         {
             _configuration = configuration; 
         }
-        [HttpPost]
-        [Route("insertpersonel")]
-        public void insertPersonel(Personel personel)
+
+        [HttpGet]
+        [Route("selectpersonel")]
+        public ActionResult<Personel> GetPersonel()
         {
-            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("PersonelConnection").ToString());
+            Personel personel = new Personel();
+
+            try
+            {
+                SqlConnection con = new SqlConnection(_configuration.GetConnectionString("PersonelConnection").ToString());
+                String cmdstring = "SELECT * FROM personel WHERE ID=(SELECT MAX(ID) FROM personel )";
+                SqlCommand cmd = new SqlCommand(cmdstring, con);
+                con.Open();
+                var dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+
+
+                    personel.Ad = dr["Ad"].ToString();
+                    personel.Soyad = dr["Soyad"].ToString();
+                    personel.ID = (int)dr["ID"];
+
+                }
+                dr.Close();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+              //  return ex.Message;
+            }
+            string message = JsonConvert.SerializeObject(personel);
+            return Content(message);
            
-            String cmdstring = "INSERT INTO personel(Ad,Soyad) VALUES(@Ad,@Soyad)";
-            SqlCommand cmd = new SqlCommand(cmdstring, con);
-            createPersonelParams(cmd, personel);
-            
-            con.Open();
-            cmd.ExecuteNonQuery();
-            con.Close();
+
         }
-        private void createPersonelParams(SqlCommand cmd,Personel personel)
+        [HttpGet]
+        [Route("{ID}")]
+        public ActionResult<Personel> getPersonelbyID(int ID)
         {
-            cmd.Parameters.AddWithValue("@Ad", personel.Ad);
-            cmd.Parameters.AddWithValue("@Soyad", personel.Soyad);
+            List<Personel> personelList = new List<Personel>();
+
+
+            try
+            {
+                SqlConnection con = new SqlConnection(_configuration.GetConnectionString("PersonelConnection").ToString());
+                String cmdstring = $"SELECT * FROM personel where ID={ID}";
+                SqlCommand cmd = new SqlCommand(cmdstring, con);
+                con.Open();
+                var dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    Personel personel = new Personel();
+
+                    personel.Ad = dr["Ad"].ToString();
+                    personel.Soyad = dr["Soyad"].ToString();
+                    personel.ID = (int)dr["ID"];
+                    personelList.Add(personel);
+
+                }
+                dr.Close();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                //  return ex.Message;
+            }
+            string message = JsonConvert.SerializeObject(personelList);
+            return Content(message);
         }
+
         
 
        
